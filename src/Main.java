b9f5f90021c6e3de0d1c0140b2eb85fd944c9a72
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-04-22 14:40:11 +0300

    Задача 4.2.5 вторая проверка

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.io.InputStream;
 public class Main {
     public static void main(String[] args) {
         byte[] array = new byte[]{1, 2, 4, 10};
-        InputStream inputStream = new ByteArrayInputStream(array);
 
-        try {
+
+        try (InputStream inputStream = new ByteArrayInputStream(array)) {
             System.out.println(SumOfStream.sumOfStream(inputStream));
         } catch (IOException e) {
             throw new RuntimeException(e);
